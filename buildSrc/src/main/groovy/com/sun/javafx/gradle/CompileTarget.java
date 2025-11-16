10294ead837095ac52784673e41daaf94747d781
Marius Hanl <mariushanl@web.de>
2025-11-16 10:56:40 +0100

    source

---
+++ buildSrc/src/main/groovy/com/sun/javafx/gradle/CompileTarget.java
@@ -0,0 +1,11 @@
+package com.sun.javafx.gradle;
+
+/**
+ * Struct used to contain some information passed to the closure
+ * passed to compileTargets.
+ */
+public class CompileTarget {
+    public String name;
+    public String upper;
+    public String capital;
+}
